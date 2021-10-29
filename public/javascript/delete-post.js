b2209cbbcd804f83cb266565166d4535d5842631
Mohamed Lazrek <mohamed.lazrekb@gmail.com>
2021-10-29 17:15:20 -0400

    form handlers

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,27 +1,27 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'DELETE',
-        body: JSON.stringify({
-          post_id: id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-  
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "DELETE",
+    body: JSON.stringify({
+      post_id: id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
 }
-  
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
 
+document
+  .querySelector(".delete-post-btn")
+  .addEventListener("click", deleteFormHandler);
---
+++ public/javascript/edit-post.js
@@ -0,0 +1,30 @@
+async function editFormHandler(event) {
+  event.preventDefault();
+
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      title,
+      post_text,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector(".edit-post-form")
+  .addEventListener("submit", editFormHandler);
